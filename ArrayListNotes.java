0528d0a83ae15d23adaec029d82f6424c324199c
mrcallaghan <mcallaghan@naperville203.org>
2025-01-07 08:30:12 -0600

    Started notes on ArrayLists
    Got through autoboxing

--- ArrayListNotes.java
+++ ArrayListNotes.java
@@ -3,20 +3,33 @@ import java.util.ArrayList;
 /**
  * Write a description of class ArrayListNotes here.
  *
- * @author gcschmit
- * @version 
+ * @author mcallaghan
+ * @version 07jan2025
  */
 public class ArrayListNotes
 {
     public static void main(String[] args)
     {
+        /*
+         * The ArrayList is a Java Generic.
+         * 
+         * We have to specify the class type of the elements in the list in angle
+         *      brackets (i.e., < >) after every ArrayList identifier.
+         *      
+         * Primitives (e.g., int, double, boolean) are not classes and cannot be
+         *      specified as the type of the element in a generic. Instead, we can
+         *      use the corresponding Wrapper Classes (e.g., Integer, Double, Boolean).
+         */
+        ArrayList<Double> myList;
+        myList = createRandomDoubleList(10, 20);
+        System.out.println(myList);
         
     }
     
     /**
      * Creates and returns a reference to an ArrayList of the specified number of
      *      Double elements where each element is assigned a random value between
-     *      1 and range.
+     *      0 and range - 1.
      *      
      *  @param size     the number of Double elements to add to the list
      *  @param range    the range of random values to assign to each element [0, range)
@@ -25,7 +38,23 @@ public class ArrayListNotes
      */
     public static ArrayList<Double> createRandomDoubleList(int size, int range)
     {
-        return null;
+        ArrayList<Double> list = new ArrayList<Double>();
+        
+        for(int i = 0; i < size; i++)
+        {
+            double value = Math.random() * range;
+            
+            /*
+             * The add method adds the specified object to the end of the list.
+             * 
+             * Autoboxing:
+             *      Primitive values are automatically converted to their corresponding
+             *          wrapper class. However, type promotion does not occur.
+             */
+            list.add(value);
+        }
+        
+        return list;
     }
          
     
